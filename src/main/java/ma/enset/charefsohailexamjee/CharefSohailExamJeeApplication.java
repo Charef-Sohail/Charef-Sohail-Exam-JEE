13b5991be9bcc4dc3d5bcfdf6db163cd7231b291
Sohail Charef <charefsohayl@gmail.com>
2026-05-11 08:51:52 +0100

    Initial commit

---
+++ src/main/java/ma/enset/charefsohailexamjee/CharefSohailExamJeeApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.charefsohailexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharefSohailExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharefSohailExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/charefsohailexamjee/CharefSohailExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.charefsohailexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharefSohailExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
